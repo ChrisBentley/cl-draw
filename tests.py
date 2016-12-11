ea4e44c0100b7341d2ac80463a734b182e7d6ba1
ChrisBentley <c.j.bentley90@gmail.com>
2016-12-11 18:50:15 +0000

    Added a first (failing) test for creating a canvas

--- tests.py
+++ tests.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 
 import unittest
+from canvas import Canvas
+
 
 class TestDraw(unittest.TestCase):
 
     def test_valid_command(self):
         pass
+
+
+class TestCanvas(unittest.TestCase):
+
+    def test_create_canvas(self):
+
+        expected_canvas = [ [ '-',  '-',  '-',  '-',  '-', '-', '-'],
+                           [ '|',  ' ',  ' ',  ' ',  ' ', ' ', '|'],
+                           [ '|',  ' ',  ' ',  ' ',  ' ', ' ', '|'],
+                           [ '|',  ' ',  ' ',  ' ',  ' ', ' ', '|'],
+                           [ '|',  ' ',  ' ',  ' ',  ' ', ' ', '|'],
+                           [ '|',  ' ',  ' ',  ' ',  ' ', ' ', '|'],
+                           [ '-',  '-',  '-',  '-',  '-', '-', '-'] ]
+
+        new_canvas = Canvas(5, 5)
+
+        self.assertEqual(expected_canvas, new_canvas.canvas)
 
 
 if __name__ == '__main__':
