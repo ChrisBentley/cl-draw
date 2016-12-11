c20aca83f7ced13f26da3bd9757f018794457b94
ChrisBentley <c.j.bentley90@gmail.com>
2016-12-11 19:08:50 +0000

    Got the canvas drawing test to pass

--- canvas.py
+++ canvas.py
@@ -8,4 +8,12 @@ class Canvas(object):
     def __init__(self, w, h):
 
         # initialize a canvas of size x by y, filled with spaces
-        self.canvas = [[' ' for x in range(w)] for y in range(h)]
+        self.canvas = [[' ' for x in range(w + 2)] for y in range(h + 2)]
+
+        # Fill in the borders of the canvas
+        for y in range(h + 2):
+            for x in range(w + 2):
+                if (y == 0 or y == (w + 1)):
+                    self.canvas[x][y] = '|'
+                if (x == 0 or x == (h + 1)):
+                    self.canvas[x][y] = '-'
