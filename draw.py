c99747aef44d8f6347161e01a3e7f032c6dfb0a1
ChrisBentley <c.j.bentley90@gmail.com>
2016-12-12 00:23:02 +0000

    Program now accepts valid commands and will run...
    the appropriate function (which are not implemented yet)

--- draw.py
+++ draw.py
@@ -36,9 +36,38 @@ def help_prompt():
     print('"B x y c" - Bucket fill which will fill the area containing the selected pixel (x,y) with the colour "c".')
     print('"Q" - Quits the draw program.\n')
 
+def print_canvas(canvas):
+
+    if (canvas == None):
+        return
+
+    print('\n')
+
+    for y in range(canvas.height):
+        for x in range(canvas.width):
+            print(canvas.canvas[y][x])
+        print('\n')
+
+    print('\n')
+
+def validate_and_create_canvas(user_input_array):
+    pass
+
+def validate_and_draw_line(user_input_array):
+    pass
+
+def validate_and_draw_rectangle(user_input_array):
+    pass
+
+def validate_and_fill_area(user_input_array):
+    pass
+
+
 def main():
 
     print('\nWelcome to draw.py. For a list of available commands you can type "help" or "h".')
+
+    canvas = None
 
     while(1):
 
@@ -49,6 +78,26 @@ def main():
         if (check_user_input(user_input_array) != True):
             # Restart the while loop if the input wasn't a valid command
             continue
+
+        command = user_input_array[0].lower()
+
+        if (command == 'q'):
+            quit()
+
+        if (command != 'c' and canvas == None):
+            print('You need to create a canvas before you can draw something.')
+            continue
+
+        if (command == 'c'):
+            canvas = validate_and_create_canvas(user_input_array)
+        elif (command == 'l'):
+            validate_and_draw_line(user_input_array)
+        elif (command == 'r'):
+            validate_and_draw_rectangle(user_input_array)
+        elif (command == 'b'):
+            validate_and_fill_area(user_input_array)
+
+        print_canvas(canvas)
 
 
 (__name__ == '__main__' and main())
